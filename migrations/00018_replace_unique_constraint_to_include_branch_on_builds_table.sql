17ba529c8edd513a22f81811057e78759f0a6de9
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-09-06 11:56:20 +0200

    drop unique constraint index with cascade

--- migrations/00018_replace_unique_constraint_to_include_branch_on_builds_table.sql
+++ migrations/00018_replace_unique_constraint_to_include_branch_on_builds_table.sql
@@ -1,8 +1,7 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 ALTER TABLE builds ADD CONSTRAINT builds_repo_source_repo_owner_repo_name_repo_branch_repo_revision_idx UNIQUE (repo_source, repo_owner, repo_name, repo_branch, repo_revision);
-DROP INDEX builds@builds_repo_source_repo_owner_repo_name_repo_revision_idx;
-ALTER TABLE builds DROP CONSTRAINT builds_repo_source_repo_owner_repo_name_repo_revision_idx;
+DROP INDEX builds@builds_repo_source_repo_owner_repo_name_repo_revision_idx CASCADE;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
