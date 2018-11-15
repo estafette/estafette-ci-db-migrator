01d301b77fe01fc378e72a34fed5b44cd594aeb5
Jorrit Salverda <jsalverda@travix.com>
2018-11-15 17:35:51 +0100

    fix default for duration

--- migrations/00027_add_duration_column_to_builds_table.sql
+++ migrations/00027_add_duration_column_to_builds_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-ALTER TABLE builds ADD duration INTERVAL '0s';
+ALTER TABLE builds ADD duration INTERVAL DEFAULT '0s';
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
