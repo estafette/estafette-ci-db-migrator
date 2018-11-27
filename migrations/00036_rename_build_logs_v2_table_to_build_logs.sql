412a4477b6ab08b09d653489ee875607408da67d
Jorrit Salverda <jsalverda@travix.com>
2018-11-27 20:53:56 +0100

    fix table rename statement

--- migrations/00036_rename_build_logs_v2_table_to_build_logs.sql
+++ migrations/00036_rename_build_logs_v2_table_to_build_logs.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-ALTER TABLE build_logs_v2 TO build_logs;
+ALTER TABLE  IF EXISTS build_logs_v2 RENAME TO build_logs;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE build_logs TO build_logs_v2;
+ALTER TABLE build_logs RENAME TO build_logs_v2;
