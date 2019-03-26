dc9d7de7605c527ebce60777be4551c685279df5
Jorrit Salverda <jsalverda@travix.com>
2019-03-26 19:40:02 +0100

    create triggers table to store triggers to act on pipeline, release and other events

---
+++ migrations/00041_create_triggers_table.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE IF NOT EXISTS triggers (
+  id INT PRIMARY KEY,
+  trigger_type VARCHAR(256),
+  identifier VARCHAR(256),
+  triggers JSONB,
+  INDEX triggers_trigger_type_identifier_idx (trigger_type, identifier),
+  INVERTED INDEX triggers_triggers (triggers)
+);
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE IF EXISTS triggers;
