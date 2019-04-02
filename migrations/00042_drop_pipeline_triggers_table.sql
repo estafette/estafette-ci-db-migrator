83c16ae281bef477f258d5d0e8719a322ff0fc47
Jorrit Salverda <jsalverda@travix.com>
2019-04-02 10:17:54 +0200

    drop pipeline_triggers table; recreate index on triggers as unique for upsert support

---
+++ migrations/00042_drop_pipeline_triggers_table.sql
@@ -0,0 +1,30 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+DROP TABLE IF EXISTS pipeline_triggers;
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+CREATE TABLE computed_pipelines (
+  id INT PRIMARY KEY DEFAULT unique_rowid(),
+	pipeline_id INT NULL,
+	repo_source STRING(256) NULL,
+	repo_owner STRING(256) NULL,
+	repo_name STRING(256) NULL,
+	repo_branch STRING(256) NULL,
+	repo_revision STRING(256) NULL,
+	build_version STRING(256) NULL,
+	build_status STRING(256) NULL,
+	labels JSONB NULL,
+	manifest STRING NULL,
+	inserted_at TIMESTAMPTZ NULL DEFAULT now():::TIMESTAMPTZ,
+	updated_at TIMESTAMPTZ NULL DEFAULT now():::TIMESTAMPTZ,
+	commits JSONB NULL,
+	duration INTERVAL NULL DEFAULT '0s':::INTERVAL,
+	release_targets JSONB NULL,
+	first_inserted_at TIMESTAMPTZ NULL DEFAULT now():::TIMESTAMPTZ,
+	INVERTED INDEX computed_pipelines_labels_idx (labels),
+	INDEX computed_pipelines_build_status_idx (build_status ASC),
+	INDEX computed_pipelines_inserted_at_idx (inserted_at ASC),
+	INDEX computed_pipelines_pipeline_id_idx (pipeline_id ASC),
+	UNIQUE INDEX computed_pipelines_order_by_idx (repo_source ASC, repo_owner ASC, repo_name ASC)
+)
