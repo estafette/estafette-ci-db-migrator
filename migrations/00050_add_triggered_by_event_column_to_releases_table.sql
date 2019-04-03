f3387dfc884d25f1d7ac2a3821e82ea968a185e9
Jorrit Salverda <jsalverda@travix.com>
2019-04-03 17:30:04 +0200

    add triggered_by_event jsonb column to releases and computed_releases table

--- migrations/00050_add_triggered_by_event_column_to_releases_table.sql
+++ migrations/00050_add_triggered_by_event_column_to_releases_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-ALTER TABLE builds ADD triggered_by JSONB;
+ALTER TABLE releases ADD triggered_by_event JSONB;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE builds DROP triggered_by;
+ALTER TABLE releases DROP triggered_by_event;
