644413ad492227dba9927ae6934b5b49333173d4
Jorrit Salverda <jsalverda@travix.com>
2019-10-13 08:02:37 +0200

    fix adding resource columns

--- migrations/00058_add_resource_columns_to_builds.sql
+++ migrations/00058_add_resource_columns_to_builds.sql
@@ -1,25 +1,21 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-ALTER TABLE
-    builds
-ADD
-    time_to_running INTERVAL DEFAULT '0s',
-    cpu_request REAL NULL,
-    cpu_limit REAL NULL,
-    cpu_max_usage REAL NULL,
-    memory_request REAL NULL,
-    memory_limit REAL NULL,
-    memory_max_usage REAL NULL;
+ALTER TABLE builds
+ADD COLUMN time_to_running INTERVAL DEFAULT '0s',
+ADD COLUMN cpu_request REAL NULL,
+ADD COLUMN cpu_limit REAL NULL,
+ADD COLUMN cpu_max_usage REAL NULL,
+ADD COLUMN memory_request REAL NULL,
+ADD COLUMN memory_limit REAL NULL,
+ADD COLUMN memory_max_usage REAL NULL;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE
-    builds
-DROP
-    time_to_running,
-    cpu_request,
-    cpu_limit,
-    cpu_max_usage,
-    memory_request,
-    memory_limit,
-    memory_max_usage;
+ALTER TABLE builds
+DROP COLUMN time_to_running,
+DROP COLUMN cpu_request,
+DROP COLUMN cpu_limit,
+DROP COLUMN cpu_max_usage,
+DROP COLUMN memory_request,
+DROP COLUMN memory_limit,
+DROP COLUMN memory_max_usage;
