98c869cc72df4b388d5c282636c27e0ce7595197
Jorrit Salverda <jsalverda@travix.com>
2019-10-13 09:39:45 +0200

    alter type from real to float instead of dropping and adding columns

--- migrations/00060_readd_resource_columns_as_float_to_builds.sql
+++ migrations/00060_readd_resource_columns_as_float_to_builds.sql
@@ -1,20 +1,12 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 ALTER TABLE builds
-DROP COLUMN cpu_request,
-DROP COLUMN cpu_limit,
-DROP COLUMN cpu_max_usage,
-DROP COLUMN memory_request,
-DROP COLUMN memory_limit,
-DROP COLUMN memory_max_usage;
-
-ALTER TABLE builds
-ADD COLUMN cpu_request FLOAT NULL,
-ADD COLUMN cpu_limit FLOAT NULL,
-ADD COLUMN cpu_max_usage FLOAT NULL,
-ADD COLUMN memory_request FLOAT NULL,
-ADD COLUMN memory_limit FLOAT NULL,
-ADD COLUMN memory_max_usage FLOAT NULL;
+ALTER cpu_request type FLOAT,
+ALTER cpu_limit type FLOAT,
+ALTER cpu_max_usage type FLOAT,
+ALTER memory_request type FLOAT,
+ALTER memory_limit type FLOAT,
+ALTER memory_max_usage type FLOAT;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
