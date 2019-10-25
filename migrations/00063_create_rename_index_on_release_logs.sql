0b4d24f4a1e310220abdb8bfadd92da90e0f1989
Jorrit Salverda <jsalverda@travix.com>
2019-10-25 09:37:05 +0200

    create indexes if they don't exists yet

--- migrations/00063_create_rename_index_on_release_logs.sql
+++ migrations/00063_create_rename_index_on_release_logs.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-CREATE INDEX release_logs_rename_idx ON release_logs (repo_source, repo_owner, repo_name);
+CREATE INDEX IF NOT EXISTS release_logs_rename_idx ON release_logs (repo_source, repo_owner, repo_name);
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
