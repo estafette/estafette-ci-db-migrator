5b44be0524117acf6b9d1f2cb38b55a0f4ea3984
Jorrit Salverda <jsalverda@travix.com>
2019-11-21 13:31:36 +0100

    create build_logs id as serial to begin with

--- migrations/00004_set_unique_rowid_for_build_logs_primary_key.sql
+++ migrations/00004_set_unique_rowid_for_build_logs_primary_key.sql
@@ -1,7 +1,5 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-ALTER TABLE build_logs ALTER id type SERIAL;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE build_logs ALTER id type INT;
