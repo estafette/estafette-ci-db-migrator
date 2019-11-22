bccb9712d14c7872d809e52f2df9354dcfbafaf6
Jorrit Salverda <jsalverda@travix.com>
2019-11-22 07:58:27 +0100

    update script 5 to get it to work for postgres

--- migrations/00005_create_build_version_table.sql
+++ migrations/00005_create_build_version_table.sql
@@ -5,9 +5,9 @@ CREATE TABLE IF NOT EXISTS build_versions (
   repo_source VARCHAR(256),
   repo_full_name VARCHAR(256),
   auto_increment INT DEFAULT 1,
-  updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  UNIQUE INDEX build_versions_repo_source_repo_full_name_idx (repo_source, repo_full_name)
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
+CREATE UNIQUE INDEX IF NOT EXISTS build_versions_repo_source_repo_full_name_idx ON build_versions (repo_source, repo_full_name);
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
