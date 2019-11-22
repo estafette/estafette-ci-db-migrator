6f9ae62ca5886e153edc8f350e88592c4b803171
Jorrit Salverda <jsalverda@travix.com>
2019-11-22 09:06:02 +0100

    update script 7 to get it to work for postgres

--- migrations/00007_create_builds_table.sql
+++ migrations/00007_create_builds_table.sql
@@ -13,9 +13,9 @@ CREATE TABLE IF NOT EXISTS builds (
   manifest TEXT,
   inserted_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  UNIQUE INDEX builds_repo_source_repo_owner_repo_name_repo_revision_idx (repo_source, repo_owner, repo_name, repo_revision),
   INVERTED INDEX builds_labels (labels)
 );
+CREATE UNIQUE INDEX IF NOT EXISTS builds_repo_source_repo_owner_repo_name_repo_revision_idx ON builds (repo_source, repo_owner, repo_name, repo_revision);
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
