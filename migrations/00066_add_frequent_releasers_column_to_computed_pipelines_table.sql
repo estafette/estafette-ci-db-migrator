d1b5dd9734795772b37fc64d9bc712cea9c73f2c
Jorrit Salverda <jsalverda@travix.com>
2020-05-25 17:25:01 +0200

    add frequent committers and releasers columns to computed_pipelines

---
+++ migrations/00066_add_frequent_releasers_column_to_computed_pipelines_table.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+ALTER TABLE computed_pipelines ADD frequent_releasers JSONB;
+CREATE INVERTED INDEX IF NOT EXISTS computed_pipelines_frequent_releasers ON computed_pipelines (frequent_releasers);
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP INDEX computed_pipelines@computed_pipelines_frequent_releasers;
+ALTER TABLE computed_pipelines DROP frequent_releasers;
