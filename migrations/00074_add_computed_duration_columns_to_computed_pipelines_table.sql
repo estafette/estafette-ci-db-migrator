d333a324977adb7add52b816b2d9438fcfcb7a4c
Jorrit Salverda <jsalverda@travix.com>
2020-05-29 08:01:32 +0200

    drop duration and time_to_running columns from builds, releases, computed_pipelines and computed_releases tables

--- migrations/00074_add_computed_duration_columns_to_computed_pipelines_table.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- SQL in this section is executed when the migration is applied.
-ALTER TABLE computed_pipelines ADD duration_running INTERVAL AS (age(updated_at, COALESCE(started_at,inserted_at))) STORED;
-ALTER TABLE computed_pipelines ADD duration_pending INTERVAL AS (age(COALESCE(started_at,inserted_at), inserted_at)) STORED;
-
--- +goose Down
--- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE computed_pipelines DROP duration_running;
-ALTER TABLE computed_pipelines DROP duration_pending;
--- migrations/00075_add_computed_duration_columns_to_computed_releases_table.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- SQL in this section is executed when the migration is applied.
-ALTER TABLE computed_releases ADD duration_running INTERVAL AS (age(updated_at, COALESCE(started_at,inserted_at))) STORED;
-ALTER TABLE computed_releases ADD duration_pending INTERVAL AS (age(COALESCE(started_at,inserted_at), inserted_at)) STORED;
-
--- +goose Down
--- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE computed_releases DROP duration_running;
-ALTER TABLE computed_releases DROP duration_pending;
