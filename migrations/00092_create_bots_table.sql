916357e167b7bd1d76c130f2d1350a7aa1781798
Jorrit Salverda <jsalverda@travix.com>
2021-05-28 15:27:00 +0200

    add bots and bot_logs tables

---
+++ migrations/00092_create_bots_table.sql
@@ -0,0 +1,28 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE IF NOT EXISTS bots (
+  id SERIAL PRIMARY KEY,
+  repo_source VARCHAR(256),
+  repo_owner VARCHAR(256),
+  repo_name VARCHAR(256),
+  triggered_by_event JSONB NULL,
+  build_status VARCHAR(256),
+  inserted_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  started_at TIMESTAMPTZ NULL,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  cpu_request FLOAT8,
+  cpu_limit FLOAT8,
+  cpu_max_usage FLOAT8,
+  memory_request FLOAT8,
+  memory_limit FLOAT8,
+  memory_max_usage FLOAT8,
+  organizations JSONB,
+  groups JSONB,
+  INDEX bots_get_pipeline_bots_idx (repo_source ASC, repo_owner ASC, repo_name ASC, inserted_at DESC),
+  INVERTED INDEX bots_organizations (organizations),
+  INVERTED INDEX bots_groups (groups)
+);
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE IF EXISTS bots;
