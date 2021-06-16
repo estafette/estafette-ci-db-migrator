9fdaad088ce3de7a967492feaec9b09243faa9f0
Jorrit Salverda <jsalverda@travix.com>
2021-06-16 08:50:45 +0200

    rename link_entity to link_id and add link_detail JSONB field to notifications

--- migrations/00094_create_notifications_table.sql
+++ migrations/00094_create_notifications_table.sql
@@ -3,13 +3,15 @@
 CREATE TABLE IF NOT EXISTS notifications (
   id SERIAL PRIMARY KEY,
   link_type VARCHAR(256),
-  link_entity VARCHAR(256),
+  link_id VARCHAR(256),
+  link_detail JSONB,
   source VARCHAR(256),
   notifications JSONB NULL,
   inserted_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
   organizations JSONB,
   groups JSONB,
-  INDEX notifications_get_link_notifications_idx (link_type ASC, link_entity ASC, inserted_at DESC),
+  INDEX notifications_get_link_notifications_idx (link_type ASC, link_id ASC, inserted_at DESC),
+  INVERTED INDEX notifications_link_detail (link_detail),
   INVERTED INDEX notifications_organizations (organizations),
   INVERTED INDEX notifications_groups (groups)
 );
