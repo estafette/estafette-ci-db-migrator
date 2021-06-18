7b73a9215b9d2ec35418d3e570d72f0cfb25cd1c
Jorrit Salverda <jsalverda@travix.com>
2021-06-18 08:59:10 +0200

    remove drop constraint, unsupported by cockroachdb

--- migrations/00019_replace_unique_constraint_to_include_branch_on_build_logs_v2_table.sql
+++ migrations/00019_replace_unique_constraint_to_include_branch_on_build_logs_v2_table.sql
@@ -2,7 +2,6 @@
 -- SQL in this section is executed when the migration is applied.
 CREATE UNIQUE INDEX build_logs_v2_repo_source_repo_owner_repo_name_repo_branch_repo_revision_idx ON build_logs_v2 (repo_source, repo_owner, repo_name, repo_branch, repo_revision);
 
-ALTER TABLE build_logs_v2 DROP CONSTRAINT build_logs_v2_repo_source_repo_owner_repo_name_repo_revision_idx;
 DROP INDEX build_logs_v2_repo_source_repo_owner_repo_name_repo_revision_idx CASCADE;
 
 -- +goose Down
