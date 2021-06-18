482b1a925abe1a34fb07a272fe51e4a0d67d6c32
Jorrit Salverda <jsalverda@travix.com>
2021-06-18 14:48:51 +0200

    do a select as fake statement

--- migrations/00090_configure_computed_pipelines_gc_ttlseconds.sql
+++ migrations/00090_configure_computed_pipelines_gc_ttlseconds.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 -- ALTER TABLE computed_pipelines CONFIGURE ZONE USING gc.ttlseconds = 3600;
-SHOW TABLES;
+SELECT id FROM builds LIMIT 1;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
 -- ALTER TABLE computed_pipelines CONFIGURE ZONE USING gc.ttlseconds = 90000;
-SHOW TABLES;
+SELECT id FROM builds LIMIT 1;
